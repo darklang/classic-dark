ca061d7f8cd7f705eca60783395a1706d2a07bc8
Stachu Korick <hello@stachu.net>
2025-08-05 11:37:48 -0400

    further telem reduction efforts

--- backend/src/QueueWorker/QueueWorker.fs
+++ backend/src/QueueWorker/QueueWorker.fs
@@ -353,7 +353,7 @@ let main _ : int =
     print "Starting QueueWorker"
     initSerializers ()
     LibService.Init.init name
-    Telemetry.Console.loadTelemetry name Telemetry.TraceDBQueries
+    Telemetry.Console.loadTelemetry name Telemetry.DontTraceDBQueries
     (LibBackend.Init.init LibBackend.Init.WaitForDB name).Result
     (LibRealExecution.Init.init name).Result
 
